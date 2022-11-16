44b730ff846cddb2185568cf019ee37cfbcc690b
nicole-esquer <nicole.esquer5@gmail.com>
2022-11-16 11:20:58 -0700

    final refactor

--- spec/facades/learning_resource_facade_spec.rb
+++ spec/facades/learning_resource_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe LearningResourceFacade do
-  xit 'creates single object combining videos and images' do
+  it 'creates single object combining videos and images' do
     response = LearningResourceFacade.learning_resource('laos')
     expect(response).to be_a(LearningResource)
   end
--- spec/requests/learning_resources_request_spec.rb
+++ spec/requests/learning_resources_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Learning Resources Request' do
-  xit 'returns response with images and video objects', :vcr do
+  it 'returns response with images and video objects', :vcr do
     get '/api/v1/learning_resources?country=laos'
 
     expect(response).to be_successful
@@ -9,7 +9,7 @@ RSpec.describe 'Learning Resources Request' do
 
     data = results[:data]
 
-    expect(data[:type]).to eq([:learning_resource])
+    expect(data[:type]).to eq("learning_resource")
     expect(data[:attributes][:country]).to be_a(String)
     expect(data[:attributes][:video]).to be_a(Hash)
     expect(data[:attributes][:images]).to be_a(Array)
